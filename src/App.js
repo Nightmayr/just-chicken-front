12df5cbd3642347452bac203cceef2ff88e2196d
Umayr <umayr23@gmail.com>
2019-01-11 16:43:03 +0000

    Added Navbar to page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Navigation from './Navigation';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
+        <Navigation/>
           <img src={logo} className="App-logo" alt="logo" />
           <p>
             Edit <code>src/App.js</code> and save to reload.
