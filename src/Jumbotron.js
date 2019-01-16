e53fbee77f64ff3f33c0969750c226dc7f9ca707
Nightmayr <umayr23@gmail.com>
2019-01-16 21:25:19 +0000

    Fixed navbar dropdown through module installs

--- src/Jumbotron.js
+++ src/Jumbotron.js
@@ -9,7 +9,7 @@ class Jumbotron extends Component{
                     <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
                     <hr class="my-4"></hr>
                     <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
-                    <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+                    <a class="btn btn-primary btn-lg" href="#" role="button">Add Route</a>
                 </div>
             </div>
         )
