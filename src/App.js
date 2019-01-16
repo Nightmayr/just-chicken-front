96e43b08cd92d42898118bc5adbfede6be034370
Nightmayr <umayr23@gmail.com>
2019-01-16 16:46:39 +0000

    Added Jumbotron component to page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 import Navbar from './Navbar';
 import Navigation from './Navigation';
+import Jumbotron from './Jumbotron';
 
 class App extends Component {
   render() {
@@ -10,20 +11,11 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           {/* <Navigation/> */}
-          <Navbar/>
-          {/* <img src={logo} className="App-logo" alt="logo" /> //replace with custom logo */}
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <Navbar />
+          {/* replace with custom logo */}
+          {/* <img src={logo} className="App-logo" alt="logo" />  */}
         </header>
+        <Jumbotron />
       </div>
     );
   }
