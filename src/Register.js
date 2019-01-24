295e8d405655591b4ded84a30ab24f45a5492c66
Nightmayr <umayr23@gmail.com>
2019-01-24 15:44:34 +0000

    Created Register page

---
+++ src/Register.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+
+class Register extends Component{
+    render() {
+        return (
+            <h1>Register Page</h1>
+        );
+    }
+}
+
+export default Register;
