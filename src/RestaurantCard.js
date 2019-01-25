6cfdf136269e3a9448414c1646982d2664fafa72
Nightmayr <umayr23@gmail.com>
2019-01-25 19:13:15 +0000

    Added axios get request to Restaurant Card component

--- src/RestaurantCard.js
+++ src/RestaurantCard.js
@@ -4,11 +4,38 @@ import {
     Route,
     Link
 } from 'react-router-dom';
-
+import axios from 'axios';
 import Show from './pages/Show';
 
 
+
 class RestaurantCard extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = { restaurant: [] };
+    }
+
+    handleBack = () => {
+        this.props.history.push('/');
+    }
+
+    handleClick = (item) => {
+        this.props.history.push('/restaurant/' + item.id);
+    }
+
+    componentDidMount() {
+        axios({
+            method: "get",
+            url: "http://localhost:8080/just-chicken/api/restaurant/getAllRestaurants",
+            responseType: "json"
+        }).then(response => {
+            this.setState({ restaurant: response.data });
+            console.log(response.data);
+        })
+    }
+
     render() {
         return (
             <Router>
@@ -18,7 +45,7 @@ class RestaurantCard extends Component {
                         <div class="card">
                             <img src={require("./logo.png")} class="card-img-top" alt="..." />
                             <div class="card-body">
-                                <h5 class="card-title">[RESTAURANT NAME]</h5>
+                                <h5 class="card-title">{this.state.restaurant}</h5>
                                 <p class="card-text">[RESTAURANT INFO]</p>
                                 {/* <a href="/" class="btn btn-primary">MORE INFO</a> */}
                             </div>
