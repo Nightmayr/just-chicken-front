badfa1baaacd80784a5d8b52b35f9822cb856f4e
Nightmayr <umayr23@gmail.com>
2019-01-28 17:03:27 +0000

    RestaurantShow page displays info associated with id

--- src/pages/Show.js
+++ src/pages/Show.js
@@ -6,16 +6,18 @@ class Show extends Component{
 
     constructor(props) {
         super(props);
-        this.state = { restaurant: [] };
+        this.state = { 
+            restaurant: ""
+         };
     }
 
     handleBack = () => {
         this.props.history.push('/restaurant');
     }
 
-    handleClick = (item) => {
-        this.props.history.push('/restaurant/' + item.id);
-    }
+    // handleClick = (item) => {
+    //     this.props.history.push('/restaurant/' + item.id);
+    // }
 
 
     componentDidMount() {
@@ -31,13 +33,28 @@ class Show extends Component{
 
 
     render() {
+           
+
+
         return (
-            <div class="container">
-                <div class="jumbotron">
+            <div>
+                 <div class="container">
+                <div class="row">
+                    <div class="col-md-3">
+                        <p class="lead">{this.state.restaurant.name}</p>
+                        <div class="list-group">
+                            <li class="list-group-item">Info 1</li>
+                            <li class="list-group-item">Info 1</li>
+                            <li class="list-group-item">Info 1</li>
+                        </div>
+                    </div>
+                    <div class="jumbotron">
 
+                    </div>
                 </div>
-
             </div>
+            </div>
+            
         );
     }
 }
