2f7b9d596b34e855ac7dcf5ebd7044b9c6c86185
Nightmayr <umayr23@gmail.com>
2019-01-28 00:53:31 +0000

    Added axios request, event handlers and JSX to Show.js

--- src/Jumbotron.js
+++ src/Jumbotron.js
@@ -4,7 +4,6 @@ import {
     Route,
     Link
 } from 'react-router-dom';
-// import New from './pages/New';
 
 
 class Jumbotron extends Component{
@@ -12,17 +11,15 @@ class Jumbotron extends Component{
         return (
             <Router>
                 <div>
-                <div class="container" id="jumboContainer">
-                    <div class="jumbotron">
-                        <h1 class="display-4">Welcome to Just-Chicken</h1>
-                        <p class="lead">View our curated list of fried chicken restaurants.</p>
-                        <hr class="my-4"></hr>
-                        <p>Don't see your favourite restaurant on the list?</p>
-                        <a class="btn btn-primary btn-lg" href="new" role="button">Add Restaurant</a>
+                    <div class="container" id="jumboContainer">
+                        <div class="jumbotron">
+                            <h1 class="display-4">Welcome to Just-Chicken</h1>
+                            <p class="lead">View our curated list of fried chicken restaurants.</p>
+                            <hr class="my-4"></hr>
+                            <p>Don't see your favourite restaurant on the list?</p>
+                            <a class="btn btn-primary btn-lg" href="new" role="button">Add Restaurant</a>
+                        </div>
                     </div>
-                </div>
-                {/* <Route exact path="/new" component={New} /> */}
-
                 </div>
             </Router>
         );
--- src/pages/Show.js
+++ src/pages/Show.js
@@ -1,10 +1,43 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 
 class Show extends Component{
+
+    constructor(props) {
+        super(props);
+        this.state = { restaurant: [] };
+    }
+
+    handleBack = () => {
+        this.props.history.push('/restaurant');
+    }
+
+    handleClick = (item) => {
+        this.props.history.push('/restaurant/' + item.id);
+    }
+
+
+    componentDidMount() {
+        axios({
+            method: "get",
+            url: "http://localhost:8080/just-chicken/api/restaurant/getRestaurant/"+this.props.match.params.id,
+            responseType: "json"
+        }).then(response => {
+            this.setState({ restaurant: response.data });
+            console.log(this.state.restaurant);
+        });
+    }
+
+
     render() {
         return (
-            <h1>Restaurant show Page</h1>
+            <div class="container">
+                <div class="jumbotron">
+
+                </div>
+
+            </div>
         );
     }
 }
