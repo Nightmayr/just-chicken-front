aacc33afd6e06a4e5a9a67f764adc08694fc015f
Nightmayr <umayr23@gmail.com>
2019-01-29 01:37:20 +0000

    Added photo to page

--- src/pages/Show.js
+++ src/pages/Show.js
@@ -2,59 +2,82 @@ import React, { Component } from 'react';
 import axios from 'axios';
 
 
-class Show extends Component{
+class Show extends Component {
 
     constructor(props) {
         super(props);
-        this.state = { 
+        this.state = {
             restaurant: ""
-         };
+        };
+
+        axios({
+            method: "get",
+            url: "http://localhost:8080/just-chicken/api/restaurant/getRestaurant/" + this.props.match.params.id,
+            responseType: "json"
+        }).then(response => {
+            this.setState({ restaurant: response.data });
+            console.log(response.data.photo);
+        });
+
+
     }
 
     handleBack = () => {
         this.props.history.push('/restaurant');
     }
 
-    // handleClick = (item) => {
-    //     this.props.history.push('/restaurant/' + item.id);
-    // }
-
-
-    componentDidMount() {
-        axios({
-            method: "get",
-            url: "http://localhost:8080/just-chicken/api/restaurant/getRestaurant/"+this.props.match.params.id,
-            responseType: "json"
-        }).then(response => {
-            this.setState({ restaurant: response.data });
-            console.log(this.state.restaurant);
-        });
+    handleClick = (item) => {
+        this.props.history.push('/restaurant/' + item.id);
     }
 
 
-    render() {
-           
+    // componentDidMount() {
+        
+    // }
 
+
+    render() {
+        const restaurantPhotoN = this.state.restaurant.photo;
+        const images = require.context('../', true);
+        const restaurantPhotoName = restaurantPhotoN;
+        // let photo = "../"+this.state.restaurant.photo;
+        console.log(restaurantPhotoName);
+
+        if (restaurantPhotoName == undefined) {
+            var restaurantPhoto = "https://via/placeholder.com/206x300";
+        } else {
+            var restaurantPhoto = images('./' + restaurantPhotoName);
+        }
+
+        // var restaurantPhoto = images('./'+restaurantPhotoName)
 
         return (
             <div>
-                 <div class="container">
-                <div class="row">
-                    <div class="col-md-3">
-                        <p class="lead">{this.state.restaurant.name}</p>
-                        <div class="list-group">
-                            <li class="list-group-item">Info 1</li>
-                            <li class="list-group-item">Info 1</li>
-                            <li class="list-group-item">Info 1</li>
+                <div class="container">
+                    <p class="lead">{this.state.restaurant.name}</p>
+                    <div class="row">
+                        <div class="col-md-3">
+                            <div class="list-group">
+                                <li class="list-group-item">Info 1</li>
+                                <li class="list-group-item">Info 1</li>
+                                <li class="list-group-item">Info 1</li>
+                            </div>
                         </div>
-                    </div>
-                    <div class="jumbotron">
+                        <div class="col-md-9">
+                            <div class="thumbnail">
+                                <img classname="img-thumbnail" src={restaurantPhoto} alt="" />
+                                <div class="caption-full">
+                                    <h4 class="pull-right">Hello</h4>
+                                    <h4> <a href=""></a> </h4>
+                                    <p>{this.state.restaurant.description}</p>
 
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
-            </div>
-            
+
         );
     }
 }
