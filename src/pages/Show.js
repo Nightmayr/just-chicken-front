a21f1e1b1a0d2a52895977b7aecfd9cd49bdb61e
Nightmayr <umayr23@gmail.com>
2019-01-29 20:05:33 +0000

    Added comment section to Show page, minor changes on New.js

--- src/pages/Show.js
+++ src/pages/Show.js
@@ -1,5 +1,10 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import {
+    BrowserRouter as Router,
+    Route,
+    Link
+} from 'react-router-dom';
 import '../ShowPage.css'
 
 
@@ -53,41 +58,48 @@ class Show extends Component {
         // var restaurantPhoto = images('./'+restaurantPhotoName)
 
         return (
-            <div>
-                <div class="container">
-                    {/* <p class="lead">{this.state.restaurant.name}</p> */}
-                    <div class="row">
-                        <div class="col-md-3">
-                            <div class="list-group">
-                                <li class="list-group-item">Address: {this.state.restaurant.address}</li>
-                                <li class="list-group-item">Info 1</li>
-                                <li class="list-group-item">Info 1</li>
+            <Router>
+                <div>
+                    <div class="container">
+                        {/* <p class="lead">{this.state.restaurant.name}</p> */}
+                        <div class="row">
+                            <div class="col-md-3">
+                                <div class="list-group">
+                                    <li class="list-group-item">Address: {this.state.restaurant.address}</li>
+                                    <li class="list-group-item">Map: </li>
+                                    {/* <li class="list-group-item">Info 1</li> */}
+                                </div>
                             </div>
-                        </div>
-                        <div class="col-md-9">
-                            {/* <div class="jumbotron"> */}
+                            <div class="col-md-9">
+                                {/* <div class="jumbotron"> */}
 
-                            <div class="thumbnail">
-                                <img classname="img-thumbnail" src={restaurantPhoto} alt="" />
-                                <div class="caption-full">
-                                    <h4 class="pull-right">{this.state.restaurant.name}</h4>
-                                    <h4> <a href=""></a> </h4>
-                                    <p>{this.state.restaurant.description}</p>
-                                    <a class="btn btn-success" href="">Update Restaurant</a>
-                                    <a class="btn btn-danger" href="">Delete Restaurant</a>
-                                </div>
-
-                                <div class="well">
-                                    <div class="text-right">
-                                        <a class="btn btn-success" href=""></a>
+                                <div class="thumbnail">
+                                    <img classname="img-thumbnail" src={restaurantPhoto} alt="" />
+                                    <div class="caption-full">
+                                        <h4 class="pull-right">{this.state.restaurant.name}</h4>
+                                        <h4> <a href=""></a> </h4>
+                                        <p>{this.state.restaurant.description}</p>
+                                        <Link class="btn btn-success" to="">Update Restaurant</Link>
+                                        <Link class="btn btn-danger" to="">Delete Restaurant</Link>
                                     </div>
+
+                                    <div class="well">
+                                        <h3 class="pull-right">Comments</h3>
+                                        {/* <div class="text-right"> */}
+                                        <div>
+                                            <hr></hr>
+                                            <p>{this.state.restaurant.comment}</p>
+                                            {/* <a class="btn btn-success" href="">New Comment</a> */}
+                                            <div></div>
+                                        </div>
+                                    </div>
+                                    {/* </div> */}
                                 </div>
-                                {/* </div> */}
                             </div>
                         </div>
                     </div>
                 </div>
-            </div>
+            </Router>
 
         );
     }
