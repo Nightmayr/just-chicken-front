cd4e49361e704949268dd9cb9926970a06154d4e
Nightmayr <umayr23@gmail.com>
2019-01-30 01:06:49 +0000

    rendered map in restaurant show page

--- src/Map.js
+++ src/Map.js
@@ -1,23 +1,23 @@
 import React, { Component } from 'react';
 import { Map, GoogleApiWrapper } from 'google-maps-react';
 
-const mapStyles = {
-  width: '100%',
-  height: '100%'
-};
+// const mapStyles = {
+//   width: '90%',
+//   height: '50%'
+// };
 
 export class MapContainer extends Component {
   render() {
-    return (
-      <Map
-        google={this.props.google}
-        zoom={14}
-        style={mapStyles}
-        initialCenter={{
-         lat: 53.473019,
-         lng: -2.297899
-        }}
-      />
+      return (
+              <Map
+                google={this.props.google}
+                zoom={17}
+                // style={mapStyles}
+                initialCenter={{
+                 lat: 53.473019,
+                 lng: -2.297899
+                }}
+              />
     );
   }
 }
