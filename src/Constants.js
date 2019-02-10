ed7e558227949ad204327350c05405a6270172d9
Nightmayr <umayr23@gmail.com>
2019-02-10 15:27:45 +0000

    added another address

--- src/Constants.js
+++ src/Constants.js
@@ -1,4 +1,9 @@
+// 1st jchicken vm
 // export const externalIP = "http://35.246.53.17:8081";
 export const externalIP = "http://nightmayr.com:8081";
+
+// 2nd jchicken vm
+// export const externalIP = "http://35.189.107.126:8081";
+
 export const local = "http://localhost:8080"; 
 
